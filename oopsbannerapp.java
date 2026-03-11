826c3e2af505e8f9a12a3878d788d496f0e6cada
sa8257 <sa8257@srmist.edu.in>
2026-03-11 09:13:28 +0530

    uc6

--- oopsbannerapp.java
+++ oopsbannerapp.java
@@ -1,19 +1,50 @@
 public class oopsbannerapp {
 
+    static String[] O() {
+        return new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+        };
+    }
+
+    static String[] P() {
+        return new String[]{
+            "****** ",
+            "*     *",
+            "*     *",
+            "****** ",
+            "*      ",
+            "*      ",
+            "*      "
+        };
+    }
+
+    static String[] S() {
+        return new String[]{
+            " ******",
+            "*      ",
+            "*      ",
+            " ***** ",
+            "      *",
+            "      *",
+            "****** "
+        };
+    }
+
     public static void main(String[] args) {
 
-        String[] banner = {
-            " *****    *****    ******    ****** ",
-            "*     *  *     *   *     *   *      ",
-            "*     *  *     *   *     *   *      ",
-            "*     *  *     *   ******    ****** ",
-            "*     *  *     *   *         *     *",
-            "*     *  *     *   *         *     *",
-            " *****    *****    *          ******"
-        };
+        String[] o1 = O();
+        String[] o2 = O();
+        String[] p  = P();
+        String[] s  = S();
 
-        for (String line : banner) {
-            System.out.println(line);
+        for(int i = 0; i < 7; i++) {
+            System.out.println(o1[i] + "  " + o2[i] + "  " + p[i] + "  " + s[i]);
         }
     }
 }
